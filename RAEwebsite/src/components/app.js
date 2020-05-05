4921d899130aaae59ea413cfdd8e88a9c96c7ec2
austin smythe <aust.burner@gmail.com>
2020-05-05 16:51:41 -0600

    added .gitignore

--- RAEwebsite/src/components/app.js
+++ RAEwebsite/src/components/app.js
@@ -4,8 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>RAE Website</h1>
-        <h2>React Redux</h2>
+        <h1>Right Angle Engineering</h1>
+        <h2>Engineering for future</h2>
       </div> 
     );
   }
